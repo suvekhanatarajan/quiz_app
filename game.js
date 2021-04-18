dec73146ee841dfd434196a2704cf197545f3d84
suvekhanatarajan <33887104+suvekhanatarajan@users.noreply.github.com>
2021-04-18 20:00:39 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,107 @@
+/**
+ *
+ */
+ const question = document.querySelector('#question');
+const choices = Array.from(document.querySelectorAll('.choice-text'));
+const progressText = document.querySelector('#progressText');
+const scoreText = document.querySelector('#score');
+const progressBarFull = document.querySelector('#progressBarFull');
+
+let currentQuestion = {}
+let acceptingAnswers = true
+let score = 0
+let questionCounter = 0
+let availableQuestions = []
+
+let questions = [
+    {
+        question: "Which datatype in java is used to create a variable that should store text?",
+        choice1: 'Text',
+        choice2: 'String',
+        choice3: 'string',
+        choice4: 'myString',
+        answer: 2,
+    },
+    {
+        question:"What does CSS stands for?",
+        choice1: "Cascading Style Sheet",
+        choice2: "Creative Style Sheet",
+        choice3: "Colorful Style Sheet",
+        choice4: "Computer Style Sheet",
+        answer: 1,
+    },
+    {
+        question: "Which HTML tag is used to define an internal Style sheet",
+        choice1: "<script>",
+        choice2: "<css>",
+        choice3: "<style>",
+        choice4: "None of the above",
+        answer: 3,
+    }
+]
+
+const SCORE_POINTS = 10
+const MAX_QUESTIONS = 3
+
+startGame = () => {
+    questionCounter = 0
+    score = 0
+    availableQuestions = [...questions]
+    getNewQuestion()
+}
+
+getNewQuestion = () => {
+    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score)
+
+        return window.location.assign('end.html')
+    }
+
+    questionCounter++
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
+    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
+   
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionsIndex]
+    question.innerText = currentQuestion.question
+
+    choices.forEach(choice => {
+        const number = choice.dataset['number']
+        choice.innerText = currentQuestion['choice' + number]
+    })
+
+    availableQuestions.splice(questionsIndex, 1)
+
+    acceptingAnswers = true
+}
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if(!acceptingAnswers) return
+
+        acceptingAnswers = false
+        const selectedChoice = e.target
+        const selectedAnswer = selectedChoice.dataset['number']
+
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
+
+        if(classToApply === 'correct') {
+            incrementScore(SCORE_POINTS)
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply)
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply)
+            getNewQuestion()
+
+        }, 1000)
+    })
+})
+
+incrementScore = num => {
+    score +=num
+    scoreText.innerText = score
+}
+
+startGame()
